dc090b96e413efd3b507327a4a1b850fa1fdd489
prernadave <prernadave3914@gmail.com>
2023-02-14 22:42:49 +0530

    skills js

---
+++ home.js
@@ -0,0 +1,59 @@
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Portfolio | Prerna Dave";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "assets/images/favhand.png");
+        }
+    });
+
+
+  
+
+
+
+//  typed js effect start
+// var typed = new typed(".typing-text", {
+//     strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+//     loop: true,
+//     typeSpeed: 50,
+//     backSpeed: 25,
+//     backDelay: 500,
+// });
+// <!-- typed js effect ends -->
+
+
+
+async function fetchData(type = "skills") {
+    let response
+    type === "skills" ?
+        response = await fetch("skills.json")
+        : 
+        response = await fetch("./projects/project.json")
+    const data = await response.json();
+    return data;
+}
+
+function showSkills(skills) {
+    let skillsContainer = document.getElementById("skills-card");
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" class="skills-card-img"/>
+                <span class="skills-card-name">${skill.name}</span>
+              </div>
+            </div>`
+    });
+    skillsContainer.innerHTML = skillHTML;
+}
+
+
+fetchData().then(data => {
+    showSkills(data);
+});
