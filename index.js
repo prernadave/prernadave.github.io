328c3dfd920251e0d5eb3b6baeafd48d3877af1e
prernadave <prernadave3914@gmail.com>
2023-05-19 12:24:56 +0530

    done

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ document.getElementById('resume-link-1').addEventListener("click",()=>{
     window.open("https://drive.google.com/file/d/1jzan2pIg7CBVGlm7XLy6kNtdvCfbGjUQ/view?usp=share_link","_blank");
   })
   
-  async function fetchData(type = "skills") {
-    let response
-    type === "skills" ?
-        response = await fetch("skills.json")
-        : 
-        response = await fetch("./projects/project.json")
-    const data = await response.json();
-    return data;
-  }
+  // async function fetchData(type = "skills") {
+  //   let response
+  //   type === "skills" ?
+  //       response = await fetch("skills.json")
+  //       : 
+  //       response = await fetch("./projects/project.json")
+  //   const data = await response.json();
+  //   return data;
+  // }
   
-  function showSkills(skills) {
-    let skillsContainer = document.getElementById("row");
-    let skillHTML = "";
-    skills.forEach(skill => {
-        skillHTML += `
-        <div class="bar">
-              <div class="info">
-                <img src=${skill.icon} alt="skill" class="skills-card-img"/>
-                <span class="skills-card-name">${skill.name}</span>
-              </div>
-            </div>`
-    });
-    skillsContainer.innerHTML = skillHTML;
-  }
+  // function showSkills(skills) {
+  //   let skillsContainer = document.getElementById("row");
+  //   let skillHTML = "";
+  //   skills.forEach(skill => {
+  //       skillHTML += `
+  //       <div class="bar">
+  //             <div class="info">
+  //               <img src=${skill.icon} alt="skill" class="skills-card-img"/>
+  //               <span class="skills-card-name">${skill.name}</span>
+  //             </div>
+  //           </div>`
+  //   });
+  //   skillsContainer.innerHTML = skillHTML;
+  // }
   
   
-  fetchData().then(data => {
-    showSkills(data);
-  });
+  // fetchData().then(data => {
+  //   showSkills(data);
+  // });
   
   
