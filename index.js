45efe5e1de07c3bb0d5d4a54c74b12ac7b3af7d4
prernadave <prernadave3914@gmail.com>
2024-02-26 13:35:18 +0530

    fixed css

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
-document.getElementById('resume-link-1').addEventListener("click",()=>{
-    window.open("./assets/Prerna_Dave_Resume (5).pdf","_blank");
-  })
+document.getElementById('resume-link-1').addEventListener("click", () => {
+  window.open("./assets/Prerna_Dave_Resume (5).pdf", "_blank");
+})
 
-  document.getElementById('resume-link-2').addEventListener("click",()=>{
-    window.open("https://drive.google.com/file/d/11o1GIF9aJjQwj77oVIVkgrgb17PjJhD_/view?usp=drive_link","_blank");
-  })
- 
-  
+document.getElementById('resume-link-2').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/11o1GIF9aJjQwj77oVIVkgrgb17PjJhD_/view?usp=drive_link", "_blank");
+})
+
+document.getElementById('resume-link-3').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/11o1GIF9aJjQwj77oVIVkgrgb17PjJhD_/view?usp=drive_link", "_blank");
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navCheck = document.getElementById("nav-check");
+  var navLinks = document.querySelectorAll(".nav-links a");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navCheck.checked = false;
+    });
+  });
+});
