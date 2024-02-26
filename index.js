d7493cdbb754e080bcfa2d1bfa6f6faf93b638ff
prernadave <prernadave3914@gmail.com>
2024-02-26 13:47:08 +0530

    added resume

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 document.getElementById('resume-link-1').addEventListener("click", () => {
-  window.open("./assets/Prerna_Dave_Resume (5).pdf", "_blank");
+  window.open("./assets/PrernaDave_fw21_0428.pdf", "_blank");
 })
 
 document.getElementById('resume-link-2').addEventListener("click", () => {
-  window.open("https://drive.google.com/file/d/11o1GIF9aJjQwj77oVIVkgrgb17PjJhD_/view?usp=drive_link", "_blank");
+  window.open("https://drive.google.com/drive/folders/1R6Zz_B8EN5PIsWvlX5Hgv98WDr8HlwSc", "_blank");
 })
 
 document.getElementById('resume-link-3').addEventListener("click", () => {
-  window.open("https://drive.google.com/file/d/11o1GIF9aJjQwj77oVIVkgrgb17PjJhD_/view?usp=drive_link", "_blank");
+  window.open("https://drive.google.com/drive/folders/1R6Zz_B8EN5PIsWvlX5Hgv98WDr8HlwSc", "_blank");
 })
 
 document.addEventListener("DOMContentLoaded", function () {
